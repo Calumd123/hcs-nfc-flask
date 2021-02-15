beef28c2d06a7c40405529c39fcce8d02ed57be7
Calum Davidson <2317831d@student.gla.ac.uk>
2021-02-15 15:09:58 +0000

    Added POST

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import sys
+from dotenv import load_dotenv
+from twilio.rest import Client
 import uuid
 from dotenv import load_dotenv
 from flask import Flask
@@ -19,3 +22,16 @@ def token():
                         grants=[SyncGrant(os.environ['TWILIO_SYNC_SERVICE_SID'])],
                         identity=uuid.uuid4().hex)
     return {'token': token.to_jwt().decode()}
+
+@app.route('/nfc', methods=['POST'])
+def pushNFC():
+
+    json_data = request.get_json(force=True)
+    text = json_data['text']
+    load_dotenv()
+    client = Client()
+
+    sync_service = client.sync.services(os.environ.get('TWILIO_SYNC_SERVICE_SID'))
+    todo_list = sync_service.sync_lists('todoList')
+    todo_list.sync_list_items.create({'todo': text})
+    return {}
